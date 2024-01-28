e2a5c3e6545b28ab8a688b30a7c8330bb2d11dea
ogzhanolguncu <ogzhan11@gmail.com>
2024-01-28 19:40:59 +0300

    Add async syn port scanner

--- src/cli_parser.rs
+++ src/cli_parser.rs
@@ -1,6 +1,9 @@
-use std::net::IpAddr;
+use core::panic;
+use std::net::{IpAddr, Ipv4Addr, ToSocketAddrs};
 
 use clap::{arg, command, Parser};
+use pnet_datalink::NetworkInterface;
+use rand::Rng;
 const MAX: u16 = 65535;
 
 #[derive(Parser, Debug)]
@@ -8,7 +11,7 @@ const MAX: u16 = 65535;
 struct Args {
     /// Target host
     #[arg(short, long, value_delimiter = ',')]
-    target: Vec<IpAddr>,
+    target_ip: Vec<Ipv4Addr>,
 
     /// Target port
     #[arg(short, long)]
@@ -21,23 +24,41 @@ struct Args {
     /// Target end port
     #[arg(short, long, default_value_t = MAX)]
     end_port: u16,
+
+    /// Target end port
+    #[arg(short, long, default_value_t = String::from("en0"))]
+    interface: String,
+
+    /// Target end port
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
 }
 
 pub enum ParseCliOutput {
     WithRange {
-        target: Vec<IpAddr>,
+        target_ip: Vec<Ipv4Addr>,
         start_port: u16,
         end_port: u16,
+        verbose: bool,
+        interface: NetworkInterface,
+        source_ip: Ipv4Addr,
+        source_port: u16,
     },
     WithTarget {
-        target: Vec<IpAddr>,
+        target_ip: Vec<Ipv4Addr>,
         port: u16,
+        verbose: bool,
+        interface: NetworkInterface,
+        source_ip: Ipv4Addr,
+        source_port: u16,
     },
 }
 
 pub fn parse_cli() -> ParseCliOutput {
     let args: Args = Args::parse();
-    let addr = args.target;
+    let addr = args.target_ip;
+    let interface = args.interface;
+    let verbose = args.verbose;
 
     if addr.is_empty() {
         panic!("You must at least give one valid address!")
@@ -47,13 +68,78 @@ pub fn parse_cli() -> ParseCliOutput {
     let end_port = args.end_port;
     let port = args.port;
 
+    let (interface, source_ip, source_port) = match fun_name(&addr, interface, start_port, end_port)
+    {
+        Ok(value) => value,
+        Err(value) => panic!("ERROR - {}", value),
+    };
+
     if let Some(port) = port {
-        return ParseCliOutput::WithTarget { target: addr, port };
+        return ParseCliOutput::WithTarget {
+            target_ip: addr,
+            port,
+            interface,
+            verbose,
+            source_ip,
+            source_port: source_port as u16,
+        };
     }
 
     return ParseCliOutput::WithRange {
-        target: addr,
+        target_ip: addr,
         start_port,
         end_port,
+        interface,
+        verbose,
+        source_ip,
+        source_port: source_port as u16,
     };
+}
+
+fn fun_name(
+    addr: &[Ipv4Addr],
+    interface: String,
+    start_port: u16,
+    end_port: u16,
+) -> Result<(NetworkInterface, Ipv4Addr, i32), &str> {
+    for addrs in addr.iter() {
+        println!(
+            "Scanning {} using SYN scan:\n*Interface: {}\n*Staring port: {}\n*Ending port: {}\n",
+            addrs, interface, start_port, end_port
+        );
+    }
+    let interface = match pnet_datalink::interfaces()
+        .iter()
+        .find(|iface| iface.name == interface)
+    {
+        Some(iface) => iface.to_owned(),
+        None => return Err("Couldn't find interface with such name"),
+    };
+    let source_port = rand::thread_rng().gen_range(10000..=65535);
+    let ip: Ipv4Addr =
+        match interface.ips.iter().find(|&entry| entry.is_ipv4()) {
+            Some(addr) => match addr.ip() {
+                IpAddr::V4(ipv4_addr) => ipv4_addr,
+                IpAddr::V6(_) => return Err(
+                    "Error happened while looking for a valid Ipv4 address in the given interface",
+                ),
+            },
+            None => return Err("No valid IPv4 addresses found for the given interface"),
+        };
+    Ok((interface, ip, source_port))
+}
+
+fn dns_lookup(hostname: &str) -> Result<Ipv4Addr, &'static str> {
+    let mut lookup_results = match format!("{hostname}:8000").to_socket_addrs() {
+        Ok(iter) => iter,
+        Err(_) => return Err("LOOKUP_FAILED"),
+    };
+    let addr = match lookup_results.next() {
+        Some(sock_addr) => match sock_addr.ip() {
+            IpAddr::V4(addr) => addr,
+            IpAddr::V6(_) => return Err("IPV6_ONLY"),
+        },
+        None => return Err("NO_ADDRS_FOUND"),
+    };
+    Ok(addr)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,241 @@
 mod cli_parser;
-use spinners::{Spinner, Spinners};
 
-use std::io::{self, Write};
-use std::net::{IpAddr, SocketAddr, TcpStream};
-use std::sync::mpsc::{channel, Sender};
+use pnet;
+use pnet::packet::tcp::MutableTcpPacket;
+use pnet::packet::Packet;
+use pnet_datalink;
+use pnet_datalink::NetworkInterface;
+use pnet_transport::{self};
+use rand::{self};
+use std::panic;
+use std::sync::Mutex;
 use std::time::Duration;
-
-use cli_parser::ParseCliOutput;
+use std::{
+    net::{IpAddr, Ipv4Addr},
+    sync::Arc,
+};
+use tokio::sync::{mpsc, Semaphore};
 use tokio::task;
+use tokio::time::sleep;
+const MAX: u16 = u16::MAX;
+
+/// Strcture for holding all the needed options/parameters
+
+struct Config {
+    target_ip: Ipv4Addr,
+    interface: NetworkInterface,
+    source_ip: Ipv4Addr,
+    source_port: u16,
+    verbose: bool,
+}
+
+/// Creates tcp packet with the SYN flag and given options
+fn create_tcp_packet(
+    buff: &mut [u8],
+    source_port: u16,
+    destination_port: u16,
+    source_ip: Ipv4Addr,
+    target_ip: Ipv4Addr,
+) -> MutableTcpPacket {
+    let mut packet = MutableTcpPacket::new(buff).unwrap();
+    packet.set_flags(pnet::packet::tcp::TcpFlags::SYN);
+    packet.set_source(source_port);
+    packet.set_destination(destination_port);
+    packet.set_window(1024);
+    packet.set_data_offset(6);
+    packet.set_sequence(rand::random::<u32>());
+    packet.set_options(&[pnet::packet::tcp::TcpOption::mss(1460)]);
+    packet.set_checksum(pnet::packet::tcp::ipv4_checksum(
+        &packet.to_immutable(),
+        &source_ip,
+        &target_ip,
+    ));
+    packet
+}
+
+async fn send_syn_packets(config: Arc<Config>, start_port: u16) -> Result<(), std::io::Error> {
+    let channel_type = pnet_transport::TransportChannelType::Layer4(
+        pnet_transport::TransportProtocol::Ipv4(pnet::packet::ip::IpNextHeaderProtocols::Tcp),
+    );
+    let (mut tx, _) = match pnet_transport::transport_channel(4096, channel_type) {
+        Ok((tx, rx)) => (tx, rx),
+        Err(e) => return Err(e),
+    };
+    let destination_port = start_port;
+    let source_port: u16 = config.source_port;
+    let source_ip = config.source_ip;
+    let target_ip = config.target_ip;
+    let mut buff: [u8; 24] = [0; 24];
+
+    let packet = create_tcp_packet(
+        &mut buff,
+        source_port,
+        destination_port,
+        source_ip,
+        target_ip,
+    );
+    match tx.send_to(packet, IpAddr::V4(target_ip)) {
+        Ok(_) => (),
+        Err(e) => return Err(e),
+    };
+    sleep(Duration::from_millis(1)).await;
+
+    Ok(())
+}
+
+/// Checks all incoming packets and finds one's that are related to the scan
+async fn handle_scan_responses(
+    tx: mpsc::Sender<u16>,
+    config: Arc<Config>,
+    is_finished: Arc<Mutex<bool>>,
+) {
+    let (_, mut receiver) = match pnet_datalink::channel(&config.interface, Default::default()) {
+        Ok(pnet_datalink::Channel::Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Wrong channel type"),
+        Err(e) => panic!("Error while creating a channel on given interface: {}", e),
+    };
+    let source_port = config.source_port;
+    let target_ip = config.target_ip;
+    loop {
+        if *is_finished.lock().unwrap() {
+            break;
+        }
+        let buf = receiver.next().unwrap();
+        let ethernet = pnet::packet::ethernet::EthernetPacket::new(buf).unwrap();
+        let ipv4 = pnet::packet::ipv4::Ipv4Packet::new(ethernet.payload()).unwrap();
+        if ipv4.get_next_level_protocol() == pnet::packet::ip::IpNextHeaderProtocols::Tcp {
+            let tcp = pnet::packet::tcp::TcpPacket::new(ipv4.payload()).unwrap();
+            if !(tcp.get_destination() == source_port && ipv4.get_source() == target_ip) {
+                continue;
+            } else {
+                if tcp.get_flags()
+                    == pnet::packet::tcp::TcpFlags::SYN + pnet::packet::tcp::TcpFlags::ACK
+                {
+                    let open_port = tcp.get_source();
+                    if config.verbose {
+                        println!("Discovered open port: {}/tcp", &open_port);
+                    };
+                    match tx.send(open_port).await {
+                        Ok(_) => (),
+                        Err(e) => panic!("Error while writing to the Sender channel: {e}"),
+                    };
+                };
+            };
+        };
+    }
+}
+
+/// Displays scan results
+fn print_results(open_ports: &[u16], target_ip: Ipv4Addr) {
+    println!(
+        "Stats: {} filtered/closed port(s) (RST or no response), {} open port(s) for {}",
+        MAX as usize - open_ports.len(),
+        open_ports.len(),
+        target_ip
+    );
+    println!("PORT\tSTATUS");
+    for port in open_ports {
+        println!("{port}\tOpen");
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let cli_inputs: ParseCliOutput = cli_parser::parse_cli();
-    let (tx, rx) = channel();
-    let mut sp = Spinner::new(Spinners::Dots9, "🔎 Scanning ports... 🔎".into());
-
-    match cli_inputs {
+    let parsed_inputs = cli_parser::parse_cli();
+    match parsed_inputs {
         cli_parser::ParseCliOutput::WithRange {
-            target,
+            target_ip,
             start_port,
             end_port,
+            verbose,
+            interface,
+            source_ip,
+            source_port,
         } => {
-            for addr in target {
-                for port in start_port..end_port {
-                    let tx: Sender<(IpAddr, u16)> = tx.clone();
-
-                    task::spawn(async move { scan(tx, port, addr).await });
-                }
+            for target in target_ip {
+                sleep(Duration::from_millis(3000)).await;
+                scan(
+                    Config {
+                        target_ip: target,
+                        interface: interface.clone(),
+                        source_ip,
+                        source_port,
+                        verbose,
+                    },
+                    start_port,
+                    end_port,
+                    false,
+                )
+                .await
             }
         }
-        cli_parser::ParseCliOutput::WithTarget { target, port } => {
-            let tx = tx.clone();
-            for addr in target {
-                let tx: Sender<(IpAddr, u16)> = tx.clone();
-
-                task::spawn(async move { scan(tx, port, addr).await });
+        cli_parser::ParseCliOutput::WithTarget {
+            target_ip,
+            port,
+            verbose,
+            interface,
+            source_ip,
+            source_port,
+        } => {
+            for target in target_ip {
+                sleep(Duration::from_millis(3000)).await;
+                scan(
+                    Config {
+                        target_ip: target,
+                        interface: interface.clone(),
+                        source_ip,
+                        source_port,
+                        verbose,
+                    },
+                    1,
+                    port,
+                    true,
+                )
+                .await
             }
-        }
-    }
-
-    let mut out: Vec<String> = vec![];
-    drop(tx);
-
-    for (ip_addr, u16) in rx {
-        out.push(format!("{}:{}", ip_addr, u16))
-    }
-
-    out.sort();
-    sp.stop();
-    //Empty space to split loader from output
-    println!("\n");
-
-    if out.is_empty() {
-        println!("Couldn't find any open port ")
-    } else {
-        for v in out {
-            println!("{} is open", v)
         }
     }
 }
 
-async fn scan(tx: Sender<(IpAddr, u16)>, start_port: u16, addr: IpAddr) {
-    match TcpStream::connect_timeout(
-        &SocketAddr::new(addr, start_port),
-        Duration::from_millis(400),
-    ) {
-        Ok(_) => {
-            io::stdout().flush().unwrap();
-            tx.send((addr, start_port)).unwrap()
+async fn scan(config: Config, start_port: u16, end_port: u16, single_port: bool) {
+    let config: Arc<Config> = Arc::new(config);
+
+    let conf_copy = Arc::clone(&config);
+    let (tx, mut rx) = mpsc::channel::<u16>(MAX as usize);
+    let scan_finished = Arc::new(Mutex::new(false));
+    let scan_finished_copy = Arc::clone(&scan_finished);
+
+    let tx_clone = tx.clone();
+    task::spawn(
+        async move { handle_scan_responses(tx_clone, conf_copy, scan_finished_copy).await },
+    );
+
+    let semaphore = Arc::new(Semaphore::new(20));
+
+    if single_port {
+        let conf: Arc<Config> = Arc::clone(&config);
+        send_syn_packets(conf, end_port).await.unwrap();
+    } else {
+        for i in start_port..end_port {
+            let conf: Arc<Config> = Arc::clone(&config);
+            let permit = Arc::clone(&semaphore).acquire_owned().await.unwrap();
+
+            task::spawn(async move {
+                send_syn_packets(conf, i).await.unwrap();
+                drop(permit);
+            });
         }
-        // If the connection is unsuccessful, do nothing. Means port is not open.
-        Err(_) => {}
     }
+
+    *scan_finished.lock().unwrap() = true;
+
+    let mut open_ports = Vec::new();
+    drop(tx);
+
+    while let Some(port) = rx.recv().await {
+        open_ports.push(port);
+    }
+
+    open_ports.sort();
+    print_results(&open_ports, config.target_ip);
 }
